f0173365d8d8553e3def778a66d7ef0944fe83e7
Ankit4Dec <ankit27866@gmail.com>
2023-07-08 03:18:44 +0530

    Another Folder add for more Questions

---
+++ Basic Coding Quesions/script.js
@@ -0,0 +1,21 @@
+/// Q1---- Callback Function
+
+function modifyArray(arr,callback){
+    arr.push(100);
+    callback();
+}
+
+var arr = [5,6,8,112,32,45,84,37,25];
+
+modifyArray(arr,function(){
+    console.log("array has been modified",arr)
+});
+
+
+/// Q2---- Reverse whole String
+
+var string = "Welcome to my YouTube Channel"
+
+var modifiedString = string.split().reverse().join();
+
+console.log(modifiedString);
